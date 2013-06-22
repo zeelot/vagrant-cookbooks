83f4e449b5a471b6412b346e8c038e72fbe7b3d6
Lorenzo Pisani <zeelot3k@gmail.com>
2013-06-22 12:16:50 -0700

    Add ability to specify users and databases to create in mysql recipe

--- dev-vm/attributes/default.rb
+++ dev-vm/attributes/default.rb
@@ -3,3 +3,5 @@ default[:vm][:foreman_version]   = "0.26.1"
 default[:vm][:bashrc][:user]     = "vagrant"
 default[:vm][:bashrc][:paths]    = []
 default[:vm][:bashrc][:envs]     = {}
+default[:vm][:mysql][:databases] = []
+default[:vm][:mysql][:users]     = []
--- dev-vm/recipes/mysql.rb
+++ dev-vm/recipes/mysql.rb
@@ -16,3 +16,27 @@ execute "add-mysql-db" do
   action :run
   ignore_failure true
 end
+
+node[:vm][:mysql][:databases].each do |database|
+  execute "add-mysql-db-#{database}" do
+    command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -e \"" +
+        "CREATE DATABASE #{database};\" " +
+        "mysql"
+    action :run
+    ignore_failure true
+  end
+end
+
+node[:vm][:mysql][:users].each do |user|
+  execute "add-mysql-user-#{user[:username]}" do
+    command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -D mysql -r -B -N -e \"CREATE USER #{user[:username]}\""
+    action :run
+    ignore_failure true
+  end
+
+  execute "grant-mysql-perms-#{user[:username]}" do
+    command "/usr/bin/mysql -u root -p#{node[:mysql][:server_root_password]} -D mysql -r -B -N -e \"GRANT ALL on *.* to #{user[:username]}\""
+    action :run
+    ignore_failure true
+  end
+end
