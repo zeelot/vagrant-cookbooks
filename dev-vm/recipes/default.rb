ddfee2d582bcc9f3669da4f7b1223d2469983018
Lorenzo Pisani <zeelot3k@gmail.com>
2013-11-30 21:51:02 -0800

    remove needless apt get update call

--- dev-vm/recipes/default.rb
+++ dev-vm/recipes/default.rb
@@ -1,8 +1,3 @@
-# For some reason, a fresh copy of my VM has issues booting up until this runs
-execute "initial-sudo-apt-get-update" do
-  command "apt-get update"
-end
-
 package "curl"
 package "vim"
 package "emacs"
